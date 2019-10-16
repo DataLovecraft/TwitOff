7e56ba65fe0cbb1a4ac1e93e4df90b2065a359eb
DataLovecraft <DataLovecraft@users.noreply.github.com>
2019-10-16 14:17:20 -0400

    updated for lecture 3

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -5,6 +5,7 @@ import os
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .twitter add_or_update_user
 
 #basedir = basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -25,6 +26,20 @@ def create_app():
     def root():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
+
+    @app.route('/user', methods=['POST', 'GET'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or reques.values['user_name']
+        try:
+            if request.method = 'POST':
+                add_or_update_user(name)
+                message = 'User {} successfully added!'.format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = 'Error adding {}: {}'.format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets, message=message)
 
     @app.route('/reset')
     def reset():
