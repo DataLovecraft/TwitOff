29503c055b758e3729b782368fcebcfd6ed1f045
DataLovecraft <DataLovecraft@users.noreply.github.com>
2019-10-18 10:01:43 -0400

    updated for lecture 4

---
+++ Procfile
@@ -0,0 +1,3 @@
+# assumes your are using pipenv
+
+web: gunicorn TWITOFF:APP -t 120 
--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,7 +1,7 @@
 '''
 Main application for TwitOff
 '''
-import os
+#import os
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
@@ -17,7 +17,7 @@ def create_app():
 
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
-    app.config['ENV'] = config('ENV') # change to production later
+    #app.config['ENV'] = config('ENV') # change to production later
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     DB.init_app(app)
@@ -27,7 +27,7 @@ def create_app():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
 
-    @app.route('/user', methods=['POST', 'GET'])
+    @app.route('/user', methods=['POST'])
     @app.route('/user/<name>', methods=['GET'])
     def user(name=None, message=''):
         name = name or reques.values['user_name']
@@ -40,6 +40,20 @@ def create_app():
             message = 'Error adding {}: {}'.format(name, e)
             tweets = []
         return render_template('user.html', title=name, tweets=tweets, message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = sorted([request.values['user1'],
+                               request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves'
+        else:
+            prediction = predict_user(user1, user2, request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
+
 
     @app.route('/reset')
     def reset():
